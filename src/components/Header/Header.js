4a998623df2da264ca9efbd3098702e922e5c225
riku <rikusumuriga@gmail.com>
2022-02-07 16:51:03 -0600

    commit test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Travel Advisor
+          Travel Advisor 
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
