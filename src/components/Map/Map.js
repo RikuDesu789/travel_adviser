7c952bd24a59c824afd4772f2d08d4e149f773bd
RIku <RIku>
2022-02-07 16:29:28 -0600

    added a map component

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutLinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 
-const Map = () =>{
-    return(
-        <h1>map</h1>
-    );
-}
+import useStyles from "./style";
 
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("min-width:600px");
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 export default Map;
